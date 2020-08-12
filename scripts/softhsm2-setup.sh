f5f0c5d0c0fe67b892fd1067b5171e62eb690152
Federico Voges <federico@hashicorp.com>
2020-08-12 15:00:47 +0100

    set permissions after token creation
    the cli tool creates the token with very restrictive permissions

--- scripts/softhsm2-setup.sh
+++ scripts/softhsm2-setup.sh
@@ -2,5 +2,6 @@
 
 apt-get install -y libltdl7 libsofthsm2 softhsm2
 mkdir -p /var/lib/softhsm/tokens/
-chmod -R g+rXw /var/lib/softhsm/tokens
 softhsm2-util --init-token --free --label 'vault' --so-pin whiteclaw --pin rulez
+# this is necessary so that the Vault process can read the files
+chmod -R g+rXw /var/lib/softhsm/tokens
